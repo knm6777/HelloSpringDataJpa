d3ae4a0c72f44cb201cc68ff6faa808bc4a4ccde
knm6777 <knm6777@gmail.com>
2021-06-12 15:20:59 +0900

    21612 first commit

---
+++ src/test/java/kr/ac/hansung/cse/hellospringdatajpa/HellospringdatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.hansung.cse.hellospringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringdatajpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
