13ab3f133772c8441ae574daa00d8b93317819a9
knm6777 <knm6777@gmail.com>
2021-06-12 15:56:06 +0900

    210612 unit test

--- src/test/java/kr/ac/hansung/cse/hellospringdatajpa/HellospringdatajpaApplicationTests.java
+++ src/test/java/kr/ac/hansung/cse/hellospringdatajpa/HellospringdatajpaApplicationTests.java
@@ -1,13 +1,50 @@
 package kr.ac.hansung.cse.hellospringdatajpa;
 
+import kr.ac.hansung.cse.hellospringdatajpa.entity.Product;
+import kr.ac.hansung.cse.hellospringdatajpa.repository.ProductRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
+@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class) // to run tests in order
 class HellospringdatajpaApplicationTests {
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Test
-    void contextLoads() {
+    @Order(1)
+    public void findProductById() {
+        Optional<Product> product = productRepository.findById(1L);
+        assertNotNull(product.get());
     }
+
+    @Test
+    @Order(2)
+    public void findAllProducts() {
+        List<Product> products = productRepository.findAll();
+        assertNotNull(products);
+    }
+
+    @Test
+    @Order(3)
+    public void createProduct() {
+        Product product = new Product("OLED TV", "LG전자", "korea", 300.0);
+        Product savedProduct = productRepository.save(product);
+
+        Product newProduct = productRepository.findById(savedProduct.getId()).get();
+        assertEquals("OLED TV", newProduct.getName());
+    }
+
 
 }
